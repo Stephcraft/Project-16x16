125e2b881908a841c092cb65434f40c4ee0bf894
Michael Carleton <micycle1@hotmail.co.uk>
2019-07-14 00:12:14 +0100

    Camera deadzone. Camera lerp to fixed position. Introduce more camera debug info. Add some random shortcuts for debugging/playing with camera. Disable JavaFX scene smoothing.

--- source/src/sidescroller/Camera.java
+++ source/src/sidescroller/Camera.java
@@ -7,80 +7,133 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 /**
- * Camera class. Extends {@link org.gicentre.utils.move.ZoomPan ZoomPan}, offering some bespoke methods relating to Project-16x16.
- * At the moment, the camera uses simple lerping when following objects.
+ * Camera class. Extends {@link org.gicentre.utils.move.ZoomPan ZoomPan},
+ * offering some bespoke methods relating to Project-16x16. At the moment, the
+ * camera uses {@link PApplet#lerp(float, float, float) lerp()} to follow
+ * objects or go to target position.
+ * 
+ * @todo: deadzone mode can be choppy when tracking + shaking
  * @author micycle1
  * @see {@link org.gicentre.utils.move.ZoomPan ZoomPan}
  */
 public final class Camera extends ZoomPan {
 
+	private SideScroller applet;
 	private float lerpSpeed = 0.05f, zoom = 1.0f;
-	private PVector position, followObjectOffset = new PVector(0, 0);
-	private boolean shaking = false, following = false;
+	private PVector targetPosition, logicalPosition, offset;
+	private PVector followObjectOffset = new PVector(0, 0), shakeOffset = new PVector(0, 0);
+	private PVector deadZoneP1, deadZoneP2;
+	private boolean shaking = false, following = false, deadZone = false;
 	private EditableObject followObject;
 	private float zoomMax = 100, zoomMin = 0;
 
 	/**
-	 * The most basic constructor.
-	 * Initialises the camera at position (0, 0).
+	 * The most basic constructor. Initialises the camera at position (0, 0).
+	 * 
 	 * @param applet Target applet ({@link SideScroller}).
 	 */
-	public Camera(PApplet applet) {
+	public Camera(SideScroller applet) {
 		super(applet);
-		position = new PVector(0, 0); // default
+		this.applet = applet;
+		targetPosition = new PVector(0, 0); // default position
+		if (SideScroller.DEBUG) {
+			applet.registerMethod("post", this);
+		}
 	}
 
 	/**
 	 * 
-	 * @param applet Target applet ({@link SideScroller}).
+	 * @param applet        Target applet ({@link SideScroller}).
 	 * @param startPosition Initial camera position.
 	 */
-	public Camera(PApplet applet, PVector startPosition) {
+	public Camera(SideScroller applet, PVector startPosition) {
 		super(applet);
-		position = new PVector(-startPosition.x, -startPosition.y);
+		this.applet = applet;
+		targetPosition = new PVector(-startPosition.x, -startPosition.y);
+		if (SideScroller.DEBUG) {
+			applet.registerMethod("post", this);
+		}
 	}
 
 	/**
 	 * 
-	 * @param applet Target applet ({@link SideScroller}).
+	 * @param applet       Target applet ({@link SideScroller}).
 	 * @param followObject Object the camera will follow.
 	 */
-	public Camera(PApplet applet, EditableObject followObject) {
+	public Camera(SideScroller applet, EditableObject followObject) {
 		super(applet);
+		this.applet = applet;
 		this.followObject = followObject;
 		following = true;
-		// setPanOffset(0, 0);
+		if (SideScroller.DEBUG) {
+			applet.registerMethod("post", this);
+		}
 	}
 
 	/**
 	 * 
-	 * @param applet Target applet ({@link SideScroller}).
+	 * @param applet       Target applet ({@link SideScroller}).
 	 * @param followObject Object the camera will follow.
-	 * @param offset Offset with which the camera will follow the given object.
+	 * @param offset       Offset with which the camera will follow the given
+	 *                     object.
 	 */
-	public Camera(PApplet applet, EditableObject followObject, PVector offset) {
+	public Camera(SideScroller applet, EditableObject followObject, PVector followOffset) {
 		super(applet);
+		this.applet = applet;
 		this.followObject = followObject;
 		following = true;
-		followObjectOffset = offset.copy();
-		// setPanOffset(0, 0);
+		followObjectOffset = followOffset.copy();
+		if (SideScroller.DEBUG) {
+			applet.registerMethod("post", this);
+		}
 	}
 
 	/**
-	 * This should be put in the {@link SideScroller#Draw draw()} loop.
+	 * Draws camera debug info if {@link SideScroller#DEBUG DEBUG} is true. Bound to
+	 * the end of draw() loop using {@link PApplet#registerMethod(String, Object)
+	 * registerMethod()} - don't call this method manually!
+	 */
+	public void post() {
+		applet.noFill();
+		applet.stroke(0, 150, 255);
+		applet.strokeWeight(2);
+		int length = 20;
+		applet.line(applet.width / 2 - length, applet.height / 2, applet.width / 2 + length, applet.height / 2);
+		applet.line(applet.width / 2, applet.height / 2 - length, applet.width / 2, applet.height / 2 + length);
+		if (following) {
+			applet.rect(getCoordToDisp(followObject.pos).x, getCoordToDisp(followObject.pos).y, length * 2, length * 2);
+			if (deadZone) {
+				applet.rectMode(PApplet.CORNER);
+				applet.rect(deadZoneP1.x, deadZoneP1.y, deadZoneP2.x - deadZoneP1.x, deadZoneP2.y - deadZoneP1.y);
+				applet.rectMode(PApplet.CENTER);
+			}
+		} else {
+			applet.rect(getCoordToDisp(PVector.mult(targetPosition, -1)).x,
+					getCoordToDisp(PVector.mult(targetPosition, -1)).y, length * 2, length * 2);
+		}
+	}
+
+	/**
+	 * Updates the camera. Should be put in the {@link SideScroller#Draw draw()}
+	 * loop.
 	 */
 	public void run() {
 		transform();
 		float scale = PApplet.lerp((float) getZoomScaleX(), zoom, lerpSpeed);
 		setZoomScaleX(scale);
 		setZoomScaleY(scale);
-		if (following) {
-			setPanOffset(PApplet.lerp(getPanOffset().x, (-followObject.pos.x - followObjectOffset.x) * zoom, lerpSpeed),
-					PApplet.lerp(getPanOffset().y, (-followObject.pos.y - followObjectOffset.y) * zoom, lerpSpeed));
-		} else {
-			// setPanOffset(PApplet.lerp(getPanOffset().x, position.x, lerpSpeed),
-			// PApplet.lerp(getPanOffset().y, position.y, lerpSpeed));
-			setPanOffset(position.x, position.y);
+		offset = new PVector(applet.width / 2, applet.height / 2);
+
+		if (following && ((deadZone && !withinDeadZone()) || !deadZone)) {
+			setPanOffset(
+					PApplet.lerp(getPanOffset().x,
+							(-followObject.pos.x - followObjectOffset.x - shakeOffset.x + offset.x) * zoom, lerpSpeed),
+					PApplet.lerp(getPanOffset().y,
+							(-followObject.pos.y - followObjectOffset.y - shakeOffset.y + offset.y) * zoom, lerpSpeed));
+		} else if (!following) {
+			setPanOffset(
+					PApplet.lerp(getPanOffset().x, (targetPosition.x - shakeOffset.x + offset.x) * zoom, lerpSpeed),
+					PApplet.lerp(getPanOffset().y, (targetPosition.y - shakeOffset.y + offset.y) * zoom, lerpSpeed));
 		}
 
 		if (shaking) {
@@ -89,8 +142,20 @@ public final class Camera extends ZoomPan {
 	}
 
 	/**
-	 * Tells camera which object to follow. retains previous offset.
-	 * @param offset
+	 * Determines whether the following-object is within deadzone.
+	 * 
+	 * @return
+	 */
+	private boolean withinDeadZone() {
+		PVector coord = applet.camera.getCoordToDisp(followObject.pos); // object screen coord
+		return withinRegion(coord, deadZoneP1, deadZoneP2);
+	}
+
+	/**
+	 * Tells the camera which object to track/follow. Retains the previous offset
+	 * (if any).
+	 * 
+	 * @param o Object to track.
 	 */
 	public void setFollowObject(EditableObject o) {
 		followObject = o;
@@ -98,8 +163,10 @@ public final class Camera extends ZoomPan {
 	}
 
 	/**
-	 * Tells camera which object to follow, and new offset.
-	 * @param offset
+	 * Tells the camera which object to track/follow, given a new offset.
+	 * 
+	 * @param o      Object to track.
+	 * @param offset Offset with which the camera will follow the given object.
 	 */
 	public void setFollowObject(EditableObject o, PVector offset) {
 		followObject = o;
@@ -108,15 +175,17 @@ public final class Camera extends ZoomPan {
 	}
 
 	/**
-	 * Modify existing follow offset.
-	 * @param followObjectOffset
+	 * Modify the existing object tracking offset.
+	 * 
+	 * @param followObjectOffset new offset
 	 */
 	public void setFollowObjectOffset(PVector followObjectOffset) {
-		this.followObjectOffset = followObjectOffset;
+		this.followObjectOffset = followObjectOffset.copy();
 	}
 
 	/**
-	 * Shake camera around current position
+	 * Shakes the camera around current position
+	 * 
 	 * @param duration Duration in frames.
 	 * @param force
 	 */
@@ -126,9 +195,44 @@ public final class Camera extends ZoomPan {
 		// and initial/return position
 	}
 
+	//
+	// coords are screen coords, not game coords todo game coord too?
+	// todo deadzone should scale with zoom?
+	/**
+	 * Defines the region in which the tracked object can move without the camera
+	 * following. When the tracked object exits the region, the camera will track
+	 * the object until it returns within the region.
+	 * 
+	 * @param point1 Coordinate 1
+	 * @param point2 Coordinate 2 (opposite corner)
+	 */
+	public void setDeadZone(PVector point1, PVector point2) {
+		deadZone = true; // auto enable
+		deadZoneP1 = point1.copy();
+		deadZoneP2 = point2.copy();
+	}
+
+	/**
+	 * Toggles the deadzone active/inactive.
+	 */
+	public void toggleDeadZone() {
+		if (deadZoneP1 != null && deadZoneP2 != null) {
+			deadZone = !deadZone;
+		} else {
+			System.err.print("Define a deadzone first");
+		}
+	}
+
+	/**
+	 * Sets camera position. Takes precedence over the follow object (if any). Could
+	 * be used to reveal a boss, then snap back to the player.
+	 * 
+	 * @param position World position camera will center on.
+	 * @see {@link ZoomPan#getDispToCoord(PVector) getDispToCoord()}
+	 */
 	public void setCameraPosition(PVector position) {
 		following = false;
-		this.position = position.copy();
+		this.targetPosition = new PVector(-position.x, -position.y);
 	}
 
 	@Override
@@ -157,10 +261,45 @@ public final class Camera extends ZoomPan {
 	}
 
 	/**
-	 * @return Formatted string representation of camera position.
+	 * Specify lerp (linear interpolation) speed for camera motion. Default = 0.02
+	 * (moves 2% towards target per frame). Smaller values provide a smoother, less
+	 * snappy, slower camera.
+	 * 
+	 * @param lerpSpeed Range = [0-1.0]
+	 * @see {@link PApplet#lerp(float, float, float) lerp()}
+	 */
+	public void setLerpSpeed(float lerpSpeed) {
+		this.lerpSpeed = lerpSpeed;
+	}
+
+	/**
+	 * Determine if a point is within rectangular region.
+	 * 
+	 * @param point PVector position to test.
+	 * @param UL    Corner one of region.
+	 * @param BR    Corner two of region (different X & Y).
+	 * @return True if point contained in region.
+	 */
+	private static boolean withinRegion(PVector point, PVector UL, PVector BR) {
+		return (point.x >= UL.x && point.y >= UL.y) && (point.x <= BR.x && point.y <= BR.y) // SE
+				|| (point.x >= BR.x && point.y >= BR.y) && (point.x <= UL.x && point.y <= UL.y) // NW
+				|| (point.x <= UL.x && point.x >= BR.x) && (point.y >= UL.y && point.y <= BR.y) // SW
+				|| (point.x <= BR.x && point.x >= UL.x) && (point.y >= BR.y && point.y <= UL.y); // NE
+	}
+
+	/**
+	 * Where is the camera in the world? Accounts for centering the camera (ie. an
+	 * object located at (0, 0) which the camera is following gives a camera
+	 * position of (0, 0) too).
+	 * 
+	 * @return Formatted string representation of camera position (point the camera
+	 *         is centered on).
+	 * @see {@link ZoomPan#getPanOffset() getPanOffset()} (not adjusted for
+	 *      centering and translation)
 	 */
 	public String getCameraPosition() {
-		return PApplet.round(getPanOffset().x) + ", " + PApplet.round(getPanOffset().y);
+		logicalPosition = PVector.sub(getPanOffset(), offset); // offset camera to center screen
+		return PApplet.round(-logicalPosition.x) + ", " + PApplet.round(-logicalPosition.y);
 	}
 
 }
--- source/src/sidescroller/SideScroller.java
+++ source/src/sidescroller/SideScroller.java
@@ -96,19 +96,28 @@ public class SideScroller extends PApplet {
 	@Override
 	public void settings() {
 		size((int) (1280 * 1.0), (int) (720 * 1.0), FX2D); // *1.5 //Changed to 16:9
-		noSmooth();
 	}
 
 	/**
-	 * setup is called once at the beginning of the game. Most variables will be initialized here. 
+	 * The default {@link #noSmooth()} does not work in FX2D mode - we override the
+	 * default function with a working technique. Cannot be placed in
+	 * {@link #settings()}, like it normally would be.
+	 */
+	@Override
+	public void noSmooth() {
+		final PSurfaceFX FXSurface = (PSurfaceFX) surface;
+		final Canvas canvas = (Canvas) FXSurface.getNative();
+		canvas.getGraphicsContext2D().setImageSmoothing(false);
+	}
+
+	/**
+	 * setup is called once at the beginning of the game. Most variables will be
+	 * initialized here.
 	 */
 	@Override
 	public void setup() {
 
-		camera = new Camera(this);
-		camera.setMouseMask(CONTROL);
-		camera.setMinZoomScale(0.3);
-		camera.setMaxZoomScale(3);
+		noSmooth();
 
 		// Start Graphics
 		background(0);
@@ -152,6 +161,14 @@ public class SideScroller extends PApplet {
 
 		// Main Load
 		load();
+
+		// Camera
+		camera = new Camera(this);
+		camera.setMouseMask(CONTROL);
+		camera.setMinZoomScale(0.3);
+		camera.setMaxZoomScale(3);
+//		camera.setDeadZone(new PVector(width * 0.25f, height * 0.25f), new PVector(width * 0.75f, height * 0.75f)); // example
+		camera.setFollowObject(player);
 	}
 
 	/**
@@ -180,10 +197,6 @@ public class SideScroller extends PApplet {
 		player.load(graphicsSheet);
 		player.pos.x = 0;
 		player.pos.y = -100;
-		camera.setFollowObject(player, new PVector(-width / 2, 0));
-
-		// Set Floor
-		// floor = 400;
 	}
 
 	public float fc = 0;
@@ -198,16 +211,16 @@ public class SideScroller extends PApplet {
 		surface.setTitle("Sardonyx Prealpha - Frame Rate " + (int) frameRate);
 
 		pushMatrix();
-		drawBelowCamera : { // drawn objects enclosed by pushMatrix() and popMatrix() are transformed by the camera.
-			// camera.setFollowObjectOffset(new PVector(mouseX - (width / 2), mouseY - (height / 2))); // todo test
-			camera.setFollowObjectOffset(new PVector(-width / 2, -height / 2)); // todo
+		drawBelowCamera: { // drawn objects enclosed by pushMatrix() and popMatrix() are transformed by the
+							// camera.
+//			camera.setFollowObjectOffset(new PVector(-width / 2, -height / 2)); // todo
 			camera.run();
 			mousePosition = camera.getMouseCoord().copy();
 			mapEditor.draw(); // Handle Draw Scene Method
 		}
 		popMatrix();
 
-		drawAboveCamera : { // Where HUD etc should be drawn
+		drawAboveCamera: { // Where HUD etc should be drawn
 			mousePosition = new PVector(mouseX, mouseY);
 			mapEditor.drawUI();
 
@@ -225,9 +238,8 @@ public class SideScroller extends PApplet {
 						5 + lineOffset * 4);
 				text("Camera Pos: " + camera.getCameraPosition(), width, 5 + lineOffset * 5);
 				text("Camera Zoom: " + String.format("%.2f", camera.getZoomScale()), width, 5 + lineOffset * 6);
-				text("World Pos: " + worldPosition.x + ", " + worldPosition.y, width, 5 + lineOffset * 7);
-				text("World Mouse: " + round(camera.getDispToCoord(new PVector(mouseX, mouseY)).x) + ", "
-						+ round(camera.getDispToCoord(new PVector(mouseX, mouseY)).y), width, 5 + lineOffset * 8);
+				text("World Mouse: " + round(camera.getMouseCoord().x) + ", " + round(camera.getMouseCoord().y), width,
+						5 + lineOffset * 7);
 			}
 		}
 
@@ -246,8 +258,8 @@ public class SideScroller extends PApplet {
 	}
 
 	/**
-	 * keyPressed decides if the key that has been pressed is a valid key.
-	 * if it is, it is then added to the keys ArrayList, and the keyPressedEvent flag is set.
+	 * keyPressed decides if the key that has been pressed is a valid key. if it is,
+	 * it is then added to the keys ArrayList, and the keyPressedEvent flag is set.
 	 */
 	@Override
 	public void keyPressed(KeyEvent event) {
@@ -256,8 +268,8 @@ public class SideScroller extends PApplet {
 	}
 
 	/**
-	 * keyReleased decides if the key pressed is valid and if it is then removes it from the 
-	 * keys ArrayList and keyReleaseEvent flag is set.
+	 * keyReleased decides if the key pressed is valid and if it is then removes it
+	 * from the keys ArrayList and keyReleaseEvent flag is set.
 	 */
 	@Override
 	public void keyReleased(KeyEvent event) {
@@ -265,22 +277,33 @@ public class SideScroller extends PApplet {
 		keyReleaseEvent = true;
 
 		switch (event.getKey()) { // must be caps
-			case 'Z' :
-				frameRate(100);
+		case 'Z':
+			frameRate(100);
+			break;
+		case 'X':
+			frameRate(10);
+			break;
+		case 'V':
+			camera.toggleDeadZone();
+			break;
+		case 'C':
+			camera.setCameraPosition(camera.getMouseCoord());
+			break;
+		case 'F':
+			camera.setFollowObject(player);
+			break;
+		default:
+			switch (event.getKeyCode()) { // non-character keys
+			case 122: // F11
+				final PSurfaceFX FXSurface = (PSurfaceFX) surface;
+				final Canvas canvas = (Canvas) FXSurface.getNative();
+				final Stage stage = (Stage) canvas.getScene().getWindow();
+				canvas.getGraphicsContext2D().setImageSmoothing(false);
+				stage.setFullScreen(!stage.isFullScreen());
 				break;
-			case 'X' :
-				frameRate(10);
-			default :
-				switch (event.getKeyCode()) { // non-character keys
-					case 122 : // F11
-						final PSurfaceFX FXSurface = (PSurfaceFX) surface;
-						final Canvas canvas = (Canvas) FXSurface.getNative();
-						final Stage stage = (Stage) canvas.getScene().getWindow();
-						stage.setFullScreen(!stage.isFullScreen());
-						break;
-					default :
-						break;
-				}
+			default:
+				break;
+			}
 		}
 	}
 
@@ -314,11 +337,11 @@ public class SideScroller extends PApplet {
 	}
 
 	/**
-	 * checks if the key pressed was valid, then returns true or false if
-	 * the key was accepted. This method is called when determining if a 
-	 * key has been pressed.
+	 * checks if the key pressed was valid, then returns true or false if the key
+	 * was accepted. This method is called when determining if a key has been
+	 * pressed.
 	 * 
-	 * @param k (int) the key that we are determining is valid and has been pressed. 
+	 * @param k (int) the key that we are determining is valid and has been pressed.
 	 * @return boolean key has or has not been pressed.
 	 */
 	public boolean keyPress(int k) {
@@ -327,6 +350,7 @@ public class SideScroller extends PApplet {
 
 	/**
 	 * Sets the scene to be used.
+	 * 
 	 * @param s the scene id.
 	 */
 	private void setScene(String s) {
@@ -337,9 +361,11 @@ public class SideScroller extends PApplet {
 	}
 
 	/**
-	 * Any object that is transformed by the camera (ie. not HUD elements) and uses mouse position in any manner should use
-	 * this method to access the <b>mouse X</b> coordinate. Such objects should not reference the PApplet's 
+	 * Any object that is transformed by the camera (ie. not HUD elements) and uses
+	 * mouse position in any manner should use this method to access the <b>mouse
+	 * X</b> coordinate. Such objects should not reference the PApplet's
 	 * {@link processing.core.PApplet.mouseX mouseX} variable.
+	 * 
 	 * @return mouseX coordinate (accounting for camera displacement)
 	 * @see {@link #getMouseY()}
 	 * @see {@link org.gicentre.utils.move.ZoomPan#getMouseCoord() getMouseCoord()}
@@ -349,9 +375,11 @@ public class SideScroller extends PApplet {
 	}
 
 	/**
-	 * Any object that is transformed by the camera (ie. not HUD elements) and uses mouse position in any manner should use
-	 * this method to access the <b>mouse Y</b> coordinate. Such objects should not reference the PApplet's 
+	 * Any object that is transformed by the camera (ie. not HUD elements) and uses
+	 * mouse position in any manner should use this method to access the <b>mouse
+	 * Y</b> coordinate. Such objects should not reference the PApplet's
 	 * {@link processing.core.PApplet.mouseX mouseY} variable.
+	 * 
 	 * @return mouseY coordinate (accounting for camera displacement)
 	 * @see {@link #getMouseX()}
 	 * @see {@link org.gicentre.utils.move.ZoomPan#getMouseCoord() getMouseCoord()}
@@ -367,6 +395,6 @@ public class SideScroller extends PApplet {
 
 	// Main
 	public static void main(String args[]) {
-		PApplet.main(new String[]{SideScroller.class.getName()});
+		PApplet.main(new String[] { SideScroller.class.getName() });
 	}
 }
