17165c478e64fe49efed656772b65ad0c32afa26
Marcin <marcin.krzykowski.2018@mumail.ie>
2019-08-03 16:24:28 +0100

    Finished the magic projectile class collision to be a line. Only thing left is ffade anim.

--- source/src/projectiles/MagicProjectile.java
+++ source/src/projectiles/MagicProjectile.java
@@ -11,6 +11,7 @@ public class MagicProjectile extends ProjectileObject {
 	final int PROJECTILE_SCALE = 4;
 	final int PROJECTILE_SPEED = 10;
 	final int ANIM_LENGTH = 8, ANIM_RATE = 4;
+	final int COLLISION_SIZE = 2;
 	final boolean IS_ANIM_LOOP = true;
 
 	public MagicProjectile(SideScroller a, int x, int y, int dir) {
@@ -28,69 +29,54 @@ public class MagicProjectile extends ProjectileObject {
 	public void display() {
 		switch (direction) {// No need writing the same can make a method to handle it works easier...
 		case LEFT:
-			displayProjectile(true, -180);// Handles the direction the projectile is flying..
+			displayProjectile(-180);// Handles the direction the projectile is flying..
+			pos.x -= speed;
+			setWidthHeight(PROJECTILE_SIZE_X * PROJECTILE_SCALE, PROJECTILE_SIZE_Y * PROJECTILE_SCALE);
 			break;
 		case RIGHT:
-			displayProjectile(false, 0);
+			displayProjectile(0);
+			pos.x += speed;
+			setWidthHeight(PROJECTILE_SIZE_X * PROJECTILE_SCALE, PROJECTILE_SIZE_Y * PROJECTILE_SCALE);
 			break;
 		case UP:
-			displayProjectile(true, -90);
+			displayProjectile(-90);
+			pos.y -= speed;
+			setWidthHeight(PROJECTILE_SIZE_X * PROJECTILE_SCALE, PROJECTILE_SIZE_Y * PROJECTILE_SCALE);
 			break;
 		case DOWN:
-			displayProjectile(true, 90);
+			displayProjectile(90);
+			pos.y += speed;
+			setWidthHeight(PROJECTILE_SIZE_X * PROJECTILE_SCALE, PROJECTILE_SIZE_Y * PROJECTILE_SCALE);
 			break;
 		}
-		debugMode(); // detection...
+		// detection...
 	}
 
 	public void update() {
-		image = animation.animate(applet.frameCount, applet.deltaTime);
 		// Destroy on collide
-		if (!hit) {
-			for (int i = 0; i < applet.collisions.size(); i++) {
-				Collision collision = applet.collisions.get(i);
-
-				if (collides(collision) && !collision.flag.equals("TRANSPARENT_BULLET")) {
-					hit = true;
-					setWidthHeight(ANIM_LENGTH * ANIM_RATE, ANIM_LENGTH * ANIM_RATE);
-					// Realistic Colliding
-					realisticColliding(collision);
-					// Setup Animation
-					setupAnimation("MAGIC::IDLE");
-					// Override Animation
-					image = animation.animate(applet.frameCount, applet.deltaTime);
-					// applet.projectileObjects.remove(this);
-				}
+		for (int i = 0; i < applet.collisions.size(); i++) {
+			Collision collision = applet.collisions.get(i);
+			if (collides(collision) && !collision.flag.equals("TRANSPARENT_BULLET")) {
+				hit = true;
+				setWidthHeight(ANIM_LENGTH * ANIM_RATE, ANIM_LENGTH * ANIM_RATE);
+				// Realistic Colliding
+				realisticColliding(collision);
+				// Setup Animation
+				setupAnimation("MAGIC::IDLE");
 			}
 		}
-		// Move Projectile
-		if (!hit) {
-			switch (direction) {
-			case LEFT:
-				pos.x -= speed;
-				setWidthHeight(PROJECTILE_SIZE_Y * PROJECTILE_SCALE, PROJECTILE_SIZE_X * PROJECTILE_SCALE);
-				break;
-			case RIGHT:
-				pos.x += speed;
-				setWidthHeight(PROJECTILE_SIZE_Y * PROJECTILE_SCALE, PROJECTILE_SIZE_X * PROJECTILE_SCALE);
-				break;
-			case UP:
-				pos.y -= speed;
-				setWidthHeight(PROJECTILE_SIZE_Y * PROJECTILE_SCALE, PROJECTILE_SIZE_X * PROJECTILE_SCALE);
-				break;
-			case DOWN:
-				pos.y += speed;
-				setWidthHeight(PROJECTILE_SIZE_Y * PROJECTILE_SCALE, PROJECTILE_SIZE_X * PROJECTILE_SCALE);
-				break;
-			}
-		}
+		debugMode(pos.x - applet.originX, pos.y - applet.originY, width, height);
+		image = animation.animate(applet.frameCount, applet.deltaTime);
+		// Override Animation
 	}
 
 	// DEBUG MODE
-	public void debugMode() {
+	public void debugMode(float x, float y, float w, float h) {
 		applet.stroke(255);
+		if (hit)
+			applet.stroke(255, 0, 0);
 		applet.noFill();
-		applet.rect(pos.x - applet.originX, pos.y - applet.originY, width, height);// For displaying collision
+		applet.rect(x, y, w, h); // For debug displaying // collision
 	}
 
 	public void hit(Collision collision) {
@@ -113,41 +99,41 @@ public class MagicProjectile extends ProjectileObject {
 		animation.start = 0;
 	}
 
-	// ---------------------------------------
 	// Setting the width and height
 	public void setWidthHeight(int w, int h) {
 		width = w;
 		height = h;
 	}
 
-	// ---------------------------------------
 	// COLLISION AND DISPLAYING OF PROJECTILE
 	// Added collision method because like it used so much...
-	public boolean isColliding(float point1, float point2, float point3) {
+	public boolean collider(float point1, float point2, float point3) {
 		return point1 - point2 < point3 - point2;
 	}
 
 	//
 	public void realisticColliding(Collision collision) {
-		if (isColliding(pos.x, applet.originX, collision.pos.x)) {
-			pos.x = collision.pos.x - collision.width / 2;
-		} else if (isColliding(collision.pos.x, applet.originX, pos.x)) {
-			pos.x = collision.pos.x + collision.width / 2;
-		} else if (isColliding(pos.y, applet.originY + height / 2, collision.pos.y)) {
-			pos.y = collision.pos.y - collision.height / 2 - height / 2;
-		} else if (isColliding(collision.pos.y, applet.originY, pos.y)) {
-			pos.y = collision.pos.y + collision.height / 2 + height / 2;
+		// LEFT
+		if (collider(pos.x, applet.originX, collision.pos.x)) {
+			pos.x = collision.pos.x - collision.width / COLLISION_SIZE;
+		} // RIGHT
+		else if (collider(collision.pos.x, applet.originX, pos.x)) {
+			pos.x = collision.pos.x + collision.width / COLLISION_SIZE;
+		} // DOWN
+		else if (collider(pos.y, applet.originY + height / COLLISION_SIZE, collision.pos.y)) {
+			pos.y = collision.pos.y - collision.height / COLLISION_SIZE - height / COLLISION_SIZE;
+		} // UP
+		else if (collider(collision.pos.y, applet.originY, pos.y)) {
+			pos.y = collision.pos.y + collision.height / COLLISION_SIZE + height / COLLISION_SIZE;
 		}
 	}
 
 	// Method for the direction of drawing the projectile ....
-	public void displayProjectile(boolean rotate, float rotation) {
+	public void displayProjectile(float rotation) {
 		applet.pushMatrix();
 		applet.translate(pos.x - applet.originX, pos.y - applet.originY);
-		if (rotate)
-			applet.rotate(PApplet.radians(rotation));
+		applet.rotate(PApplet.radians(rotation));
 		applet.image(image, 0, 0);
 		applet.popMatrix();
 	}
-	// ---------------------------------------
 }
--- source/src/sidescroller/GameGraphics.java
+++ source/src/sidescroller/GameGraphics.java
@@ -1,4 +1,4 @@
-package sidescroller;
+	package sidescroller;
 
 import java.util.ArrayList;
 
