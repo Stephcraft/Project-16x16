99a3f60f83bcd6930328f30e3ef4671f63f6304d
Trytoon <38983022+Trytoon@users.noreply.github.com>
2019-08-25 14:03:51 +0200

    Add files via upload

--- source/src/scene/SceneMapEditor.java
+++ source/src/scene/SceneMapEditor.java
@@ -10,15 +10,12 @@ import processing.core.*;
 import processing.event.MouseEvent;
 import projectiles.ProjectileObject;
 import scene.components.WorldViewportEditor;
+import sidescroller.GameGraphics.Graphic;
 import sidescroller.SideScroller;
-import sidescroller.Tileset;
-import sidescroller.Tileset.tileType;
+import windows.OptionsInterface;
 import ui.Anchor;
 import ui.ScrollBarVertical;
-import ui.Tab;
-import windows.LoadTestWindow;
 import windows.SaveLevelWindow;
-import windows.TestWindow;
 
 public class SceneMapEditor extends PScene {
 
@@ -27,6 +24,25 @@ public class SceneMapEditor extends PScene {
 	private PImage slotEditor;
 
 	// Graphics Icon
+	PGraphics icon_eye;
+	PGraphics icon_arrow;
+	PGraphics icon_inventory;
+	PGraphics icon_play;
+	PGraphics icon_save;
+	PGraphics icon_options;
+	PGraphics icon_eyeActive;
+	PGraphics icon_arrowActive;
+	PGraphics icon_inventoryActive;
+	PGraphics icon_playActive;
+	PGraphics icon_saveActive;
+	PGraphics icon_optionsActive;
+	PGraphics icon_leavePlayMode;
+	PGraphics icon_leavePlayModeActive;
+
+	// Windows
+	public SaveLevelWindow window_saveLevel;
+	public OptionsInterface window_options;
+
 	private PImage icon_eye;
 	private PImage icon_arrow;
 	private PImage icon_inventory;
@@ -40,14 +56,7 @@ public class SceneMapEditor extends PScene {
 
 	// Windows
 	private SaveLevelWindow window_saveLevel;
-	private LoadTestWindow window_loadTest;
-	private TestWindow window_test;
 
-	//Tabs
-	private Tab windowTabs;
-	//Each button id corresponds with its string id: ex) load = 0, save = 1, etc.
-	String tabTexts[] = new String[] {"load", "save", "long name"};
-	
 	// Editor Item
 	private EditorItem editorItem;
 
@@ -58,10 +67,10 @@ public class SceneMapEditor extends PScene {
 	private ScrollBarVertical scrollBar;
 
 	public enum Tools {
-		MOVE, MODIFY, INVENTORY, PLAY, SAVE, LOADEXAMPLE, TEST,
+		MOVE, MODIFY, INVENTORY, PLAY, SAVE, OPTIONS,
 	}
 
-	public Tools tool;
+	public static Tools tool;
 
 	private ArrayList<String> inventory;
 
@@ -97,23 +106,29 @@ public class SceneMapEditor extends PScene {
 		slot = util.pg(applet.graphicsSheet.get(289, 256, 20, 21), 4);
 		slotEditor = util.pg(applet.graphicsSheet.get(310, 256, 20, 21), 4);
 
-		// Get Icon Graphics
+		// Get Icon Graphics from graphics-sheet.png
+
 		icon_eye = util.pg(applet.graphicsSheet.get(267, 302, 11, 8), 4);
 		icon_arrow = util.pg(applet.graphicsSheet.get(279, 301, 9, 9), 4);
 		icon_inventory = util.pg(applet.graphicsSheet.get(289, 301, 9, 9), 4);
 		icon_play = util.pg(applet.graphicsSheet.get(298, 301, 9, 9), 4);
 		icon_save = util.pg(applet.graphicsSheet.get(307, 301, 9, 9), 4);
+		icon_options = util.pg(applet.graphicsSheet.get(327, 302, 8, 8), 4);
+
 
 		icon_eyeActive = util.pg(applet.graphicsSheet.get(267, 292, 11, 8), 4);
 		icon_arrowActive = util.pg(applet.graphicsSheet.get(279, 291, 9, 9), 4);
 		icon_inventoryActive = util.pg(applet.graphicsSheet.get(289, 291, 9, 9), 4);
 		icon_playActive = util.pg(applet.graphicsSheet.get(298, 291, 9, 9), 4);
 		icon_saveActive = util.pg(applet.graphicsSheet.get(307, 291, 9, 9), 4);
+		icon_optionsActive = util.pg(applet.graphicsSheet.get(327, 292, 8, 8), 4);
+		
+		icon_leavePlayMode = util.pg(applet.graphicsSheet.get(306, 321, 9, 9), 4); 
+		icon_leavePlayModeActive = util.pg(applet.graphicsSheet.get(306, 311, 9, 9), 4); 
 
 		// Init Window
 		window_saveLevel = new SaveLevelWindow(applet);
-		window_loadTest = new LoadTestWindow(applet);
-		window_test = new TestWindow(applet);
+		window_options = new OptionsInterface(applet);
 
 		// Init ScollBar
 		Anchor scrollBarAnchor = new Anchor(applet, -20, 150, 20, 50);
@@ -128,10 +143,8 @@ public class SceneMapEditor extends PScene {
 		tool = Tools.MODIFY;
 
 		util.loadLevel(SideScroller.LEVEL); // TODO change level
-		
-		windowTabs = new Tab(applet, tabTexts, 3);
 	}
-	
+
 	/**
 	 * Draw scene elements that are below (affected by) the camera.
 	 */
@@ -141,6 +154,10 @@ public class SceneMapEditor extends PScene {
 
 		applet.noStroke();
 		applet.fill(29, 33, 45);
+
+		//		applet.rect(applet.worldPosition.x, applet.worldPosition.y, applet.worldWidth,
+		//				applet.worldHeight); // todo
+
 
 		if (tool == Tools.MODIFY) {
 			displayGrid();
@@ -208,6 +225,24 @@ public class SceneMapEditor extends PScene {
 			}
 		}
 
+		// Editor View
+		if (tool == Tools.MODIFY) {
+			for (int i = 0; i < applet.collisions.size(); i++) {
+				applet.collisions.get(i).displayEdit();
+			}
+			for (int i = 0; i < applet.backgroundObjects.size(); i++) {
+				applet.backgroundObjects.get(i).displayEdit();
+			}
+			for (int i = 0; i < applet.gameObjects.size(); i++) {
+				applet.gameObjects.get(i).displayEdit();
+			}
+		}
+
+		// Editor Object Destination
+		if (tool == Tools.MODIFY) {
+			editorItem.displayDestination();
+		}
+
 		// View Projectiles
 		Iterator<ProjectileObject> i = applet.projectileObjects.iterator();
 		while (i.hasNext()) {
@@ -235,12 +270,15 @@ public class SceneMapEditor extends PScene {
 			case MOVE :
 			case INVENTORY :
 			case SAVE :
-			case LOADEXAMPLE :
-			case TEST :
 				break;
 			default :
 				break;
 		}
+		if (tool == Tools.PLAY) {
+			applet.player.updateEdit();
+			applet.player.displayEdit();
+		}
+
 		applet.player.display();
 
 		// View Viewport Editor
@@ -260,7 +298,7 @@ public class SceneMapEditor extends PScene {
 				image(slot, 20 * 4 / 2 + 10 + i * (20 * 4 + 10), 20 * 4 / 2 + 10);
 
 				// Display Item
-				PImage img = Tileset.getTile(inventory.get(i));
+				PImage img = applet.gameGraphics.get(inventory.get(i));
 				applet.image(img, 20 * 4 / 2 + 10 + i * (20 * 4 + 10), 20 * 4 / 2 + 10, img.width * (float) 0.5,
 						img.height * (float) 0.5);
 
@@ -272,56 +310,115 @@ public class SceneMapEditor extends PScene {
 							&& applet.getMouseY() > y - (20 * 4) / 2 && applet.getMouseY() < y + (20 * 4) / 2) {
 						editorItem.focus = true;
 						editorItem.setTile(inventory.get(i));
-						editorItem.type = Tileset.getTileType(inventory.get(i));
+						editorItem.type = applet.gameGraphics.getType(inventory.get(i));
 					}
 				}
 			}
 		}
 
 		// GUI Icons
-		if (tool == Tools.MOVE || (util.hover(40, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
-			if (util.hover(40, 120, 36, 36) && applet.mousePressEvent) {
-				tool = Tools.MOVE;
+		if(tool != Tools.PLAY) {
+			if (tool == Tools.MOVE || (util.hover(40, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(40, 120, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.MOVE;
+				}
+				image(icon_eyeActive, 40, 120);
+			} else {
+				image(icon_eye, 40, 120);
 			}
-			image(icon_eyeActive, 40, 120);
-		} else {
-			image(icon_eye, 40, 120);
-		}
-		if (tool == Tools.MODIFY || (util.hover(90, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
-			if (util.hover(90, 120, 36, 36) && applet.mousePressEvent) {
-				tool = Tools.MODIFY;
+			if (tool == Tools.MODIFY || (util.hover(90, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(90, 120, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.MODIFY;
+				}
+				image(icon_arrowActive, 90, 120);
+			} else {
+				image(icon_arrow, 90, 120);
 			}
-			image(icon_arrowActive, 90, 120);
-		} else {
-			image(icon_arrow, 90, 120);
-		}
-		if (tool == Tools.INVENTORY
-				|| (util.hover(90 + 48, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
-			if (util.hover(90 + 48, 120, 36, 36) && applet.mousePressEvent) {
-				tool = Tools.INVENTORY;
+			if (tool == Tools.INVENTORY
+					|| (util.hover(90 + 48, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(90 + 48, 120, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.INVENTORY;
+				}
+				image(icon_inventoryActive, 90 + 48, 120);
+			} else {
+				image(icon_inventory, 90 + 48, 120);
 			}
-			image(icon_inventoryActive, 90 + 48, 120);
-		} else {
-			image(icon_inventory, 90 + 48, 120);
-		}
-		if (tool == Tools.PLAY
-				|| (util.hover(90 + 48 * 2, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
-			if (util.hover(90 + 48 * 2, 120, 36, 36) && applet.mousePressEvent) {
-				tool = Tools.PLAY;
+			if (tool == Tools.PLAY
+					|| (util.hover(90 + 48 * 2, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(90 + 48 * 2, 120, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.PLAY;
+				}
+				image(icon_playActive, 90 + 48 * 2, 120);
+			} else {
+				image(icon_play, 90 + 48 * 2, 120);
 			}
-			image(icon_playActive, 90 + 48 * 2, 120);
-		} else {
-			image(icon_play, 90 + 48 * 2, 120);
-		}
-		if (tool == Tools.SAVE
-				|| (util.hover(90 + 48 * 3, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
-			if (util.hover(90 + 48 * 3, 120, 36, 36) && applet.mousePressEvent) {
-				tool = Tools.SAVE;
+			if (tool == Tools.SAVE
+					|| (util.hover(90 + 48 * 3, 120, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(90 + 48 * 3, 120, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.SAVE;
+				}
+				image(icon_saveActive, 90 + 48 * 3, 120);
+			} else {
+				image(icon_save, 90 + 48 * 3, 120);
 			}
-			image(icon_saveActive, 90 + 48 * 3, 120);
-		} else {
-			image(icon_save, 90 + 48 * 3, 120);
+		}else {
+			if(tool != Tools.PLAY
+					|| (util.hover(80, 850, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+				if (util.hover(80, 850, 36, 36) && applet.mousePressEvent) {
+					tool = Tools.MOVE;
+				}
+				image(icon_leavePlayModeActive, 80, 850);
+				applet.text("Leave Play Mode",  172, 820);
+			} else {
+				image(icon_leavePlayMode, 80, 850);
+			}
 		}
+		if(tool == Tools.OPTIONS
+				|| (util.hover(90 + 48 * 30, 850, 36, 36) && tool != Tools.SAVE && tool != Tools.INVENTORY)) {
+			if (util.hover(90 + 48 * 30, 850, 36, 36) && applet.mousePressEvent) {
+				tool = Tools.OPTIONS;
+			}
+			image(icon_optionsActive, 90 + 48 * 30, 850);
+			applet.text("Option Menu",  1585, 820);
+		} else {
+			image(icon_options, 90 + 48 * 30, 850);
+		}
+
+
+		// GUI Editor Object
+		if (tool == Tools.MODIFY) {
+			editorItem.update();
+			editorItem.display();
+		}
+
+		// Display Inventory
+		if (tool == Tools.INVENTORY) {
+			displayCreativeInventory();
+		}
+
+		// Windows
+		if (tool == Tools.SAVE) {
+			window_saveLevel.update();
+			window_saveLevel.display();
+		}
+
+		if(tool == Tools.OPTIONS)
+			window_options.display();
+		window_options.update();
+		window_options.displayOptions();
+
+		// Display player health
+		// TODO player health	
+
+
+		// Move Tool
+		if (tool == Tools.MOVE) {
+			if (applet.mousePressed) {
+				//				applet.originTargetX += applet.pmouseX - applet.getMouseX();
+				//				applet.originTargetY += applet.pmouseY - applet.getMouseY();
+				//				applet.originX = applet.originTargetX;
+				//				applet.originY = applet.originTargetY;
+			}
 
 		switch (tool) {
 			case INVENTORY :
@@ -336,57 +433,8 @@ public class SceneMapEditor extends PScene {
 			case PLAY :
 				break;
 			case SAVE :
-				//The if statement below should be used in each window that includes a tab. switch the number to the id of the button it's checking for
-				if(windowTabs.getActiveButton() != 1) {
-					windowTabs.moveActive(1);
-				}
-				window_saveLevel.privacyDisplay();
-				windowTabs.update();
-				windowTabs.display();
 				window_saveLevel.update();
 				window_saveLevel.display();
-				// This is an example of how to switch windows when another tab button is pressed.
-				if(windowTabs.getButton(0).event()) {
-					windowTabs.moveActive(0);
-					tool = Tools.LOADEXAMPLE;
-				} else if(windowTabs.getButton(2).event()) {
-					windowTabs.moveActive(2);
-					tool = Tools.TEST;
-				}
-				break;
-			case LOADEXAMPLE :
-				if(windowTabs.getActiveButton() != 0) {
-					windowTabs.moveActive(0);
-				}
-				window_loadTest.privacyDisplay();
-				windowTabs.update();
-				windowTabs.display();
-				window_loadTest.update();
-				window_loadTest.display();
-				if(windowTabs.getButton(1).event()) {
-					windowTabs.moveActive(1);
-					tool = Tools.SAVE;
-				} else if(windowTabs.getButton(2).event()) {
-					windowTabs.moveActive(2);
-					tool = Tools.TEST;
-				}
-				break;
-			case TEST :
-				if(windowTabs.getActiveButton() != 2) {
-					windowTabs.moveActive(2);
-				}
-				window_test.privacyDisplay();
-				windowTabs.update();
-				windowTabs.display();
-				window_test.update();
-				window_test.display();
-				if(windowTabs.getButton(0).event()) {
-					windowTabs.moveActive(0);
-					tool = Tools.LOADEXAMPLE;
-				} else if(windowTabs.getButton(1).event()) {
-					windowTabs.moveActive(1);
-					tool = Tools.SAVE;
-				}
 				break;
 			default :
 				break;
@@ -433,6 +481,16 @@ public class SceneMapEditor extends PScene {
 						scroll_inventory = 0;
 					}
 				}
+				if(applet.keyPress(79)) {
+					if(tool != Tools.OPTIONS) {
+						tool = Tools.OPTIONS;
+						editorItem.setMode("CREATE");
+						editorItem.focus = false;
+					}
+					else {
+						tool = Tools.MOVE;
+					}
+				}
 			}
 		}
 	}
@@ -448,9 +506,7 @@ public class SceneMapEditor extends PScene {
 		int x = 0;
 		int y = 1;
 		int index = 0;
-		tileType[] tiles = {tileType.COLLISION, tileType.BACKGROUND, tileType.OBJECT};
-		ArrayList<PImage> inventoryTiles = Tileset.getAllTiles(tiles);
-		for (PImage img : inventoryTiles) {
+		for (Graphic g : applet.gameGraphics.graphics.values()) {
 			if (index % 6 == 0) { // show 6 items per row
 				x = 0;
 				y++;
@@ -458,6 +514,7 @@ public class SceneMapEditor extends PScene {
 				x++;
 			}
 			applet.image(slotEditor, 20 * 4 / 2 + 10 + x * (20 * 4 + 10), y * (20 * 4 + 10) + scroll_inventory);
+			PImage img = g.image;
 			if (img.width > 20 * 4 || img.height > 20 * 4) {
 				applet.image(img, 20 * 4 / 2 + 10 + x * (20 * 4 + 10), y * (20 * 4 + 10) + scroll_inventory,
 						img.width / 4, img.height / 4);
@@ -474,7 +531,7 @@ public class SceneMapEditor extends PScene {
 					if (applet.getMouseX() > xx - (20 * 4) / 2 && applet.getMouseX() < xx + (20 * 4) / 2
 							&& applet.getMouseY() > yy - (20 * 4) / 2 && applet.getMouseY() < yy + (20 * 4) / 2) {
 						editorItem.focus = true;
-						editorItem.setTile(Tileset.getTileName(Tileset.getTileId(img)));
+						editorItem.setTile(g.name);
 					}
 				}
 			}
@@ -497,12 +554,14 @@ public class SceneMapEditor extends PScene {
 		applet.line(0, 100, applet.width, 100);
 
 		// Display Inventory Slots
+
 		for (int i = 0; i < 6; i++) {
 			// Display Slot
 			image(slot, 20 * 4 / 2 + 10 + i * (20 * 4 + 10), 20 * 4 / 2 + 10);
 
+
 			// Display Item
-			PImage img = Tileset.getTile(inventory.get(i));
+			PImage img = applet.gameGraphics.get(inventory.get(i));
 			applet.image(img, 20 * 4 / 2 + 10 + i * (20 * 4 + 10), 20 * 4 / 2 + 10, img.width * (float) 0.5,
 					img.height * (float) 0.5);
 
@@ -526,6 +585,25 @@ public class SceneMapEditor extends PScene {
 	private void displayGrid() {// world edit grid
 		applet.strokeWeight(2);
 		applet.stroke(50);
+		int x = 0;
+		int y = 0;
+		int l = applet.width / (16 * 4) * applet.height / (16 * 4);
+		for (int i = 0; i < l; i++) {
+
+			x++;
+			if (i % applet.height / (16 * 4) == 0) {
+				y++;
+				x = 0;
+			}
+			//			applet.line(x * (4 * 16) - (applet.originX % (16 * 4)) - ((4 * 16) / 2), 0,
+			//					x * (4 * 16) - (applet.originX % (16 * 4)) - ((4 * 16) / 2), applet.height);
+			//			applet.line(0, y * (4 * 16) - (applet.originY % (16 * 4)) - ((4 * 16) / 2), applet.width,
+			//					y * (4 * 16) - (applet.originY % (16 * 4)) - ((4 * 16) / 2)); // todo
+
+			applet.line(x * (4 * 16) - (0 % (16 * 4)) - ((4 * 16) / 2), 0,
+					x * (4 * 16) - (0 % (16 * 4)) - ((4 * 16) / 2), applet.height);
+			applet.line(0, y * (4 * 16) - (0 % (16 * 4)) - ((4 * 16) / 2), applet.width,
+					y * (4 * 16) - (0 % (16 * 4)) - ((4 * 16) / 2)); // todo
 		final int xOffset = 32; // to align with rectMode(CENTER)
 		final int yOffset = 32; // to align with rectMode(CENTER)
 		final int l = 6400;
@@ -538,9 +616,7 @@ public class SceneMapEditor extends PScene {
 	private float getInventorySize() {
 		int y = 1;
 
-		tileType[] tiles = {tileType.COLLISION, tileType.BACKGROUND, tileType.OBJECT};
-		ArrayList<PImage> inventoryTiles = Tileset.getAllTiles(tiles);
-		for (int i = 0; i < inventoryTiles.size(); i++) {
+		for (int i = 0; i < applet.gameGraphics.graphics.size(); i++) {
 			if (i % 6 == 0) {
 				y++;
 			} else {
