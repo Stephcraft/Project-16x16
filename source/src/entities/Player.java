ba501de9f4f0345dde14fb80a77f20adf0cde533
Quillbert <quillbert.quillbert@gmail.com>
2019-09-11 23:22:18 -0400

    added comments

--- source/src/entities/Player.java
+++ source/src/entities/Player.java
@@ -146,6 +146,8 @@ public final class Player extends EditableObject {
 	 * The update method handles updating the character.
 	 */
 	public void update() {
+		// If falling, flying = true, triggering falling animation
+		// This should fix the walking of edge animation problem
 		if(pos.y > py) {
 			flying = true;
 		}
